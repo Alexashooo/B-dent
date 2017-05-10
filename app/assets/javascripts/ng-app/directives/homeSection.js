ce91f4555b0376a2b1b4fb91c42a36db4a422f61
Aleksandar Sunjic <aleksandar.sunjic@gmail.com>
2017-05-10 13:37:01 +0200

    More improvements + home page

--- app/assets/javascripts/ng-app/directives/homeSection.js
+++ app/assets/javascripts/ng-app/directives/homeSection.js
@@ -10,6 +10,27 @@
 
           scope.imagesHome = ImageSharing.home.desk;
 
+          var imageContainer = $('.image-home-container');
+          var img = element.find('img');
+
+
+          var imageAdjHeightWidth = function(){
+              if(imageContainer.height() >= imageContainer.width()){
+                   img.removeClass('image-home-wider').addClass('image-home-higher');
+
+              }else{
+                   img.removeClass('image-home-higher').addClass('image-home-wider');
+              }
+          };
+
+          img.bind('load',function(){
+                imageAdjHeightWidth();
+          });
+
+          $(window).on("resize", function(){
+                imageAdjHeightWidth();
+          });
+
           }
 
         }
