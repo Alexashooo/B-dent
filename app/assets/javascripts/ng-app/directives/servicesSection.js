1f4cdb6415c2ce6ab4226a306925dba4d2bdd4a3
Aleksandar Sunjic <aleksandar.sunjic@gmail.com>
2017-05-11 15:50:29 +0200

    Better responsivness

--- app/assets/javascripts/ng-app/directives/servicesSection.js
+++ app/assets/javascripts/ng-app/directives/servicesSection.js
@@ -9,7 +9,31 @@
 
           link: function(scope, element, attrs){
 
-          scope.imagesServices = ImageSharing.services.desk;
+                scope.imagesServices = ImageSharing.services.desk;
+
+                var imageContainer = element.find('.service-box');
+                    textBox = element.find('.text-box');
+                    img = element.find('img');
+
+
+                var imageAdjHeightWidth = function(){
+                    if(imageContainer.height() >= imageContainer.width()){
+                         img.removeClass('service-images-wider').addClass('service-images-higher');
+                    }else{
+                         img.removeClass('service-images-higher').addClass('service-images-wider');
+                    }
+                    textBox.css({height:img.height(), width:img.width()});
+                };
+
+                img.bind('load',function(){
+                      imageAdjHeightWidth();
+                });
+
+                $(window).on("resize", function(){
+                      imageAdjHeightWidth();
+                });
+
+
 
           }
 
